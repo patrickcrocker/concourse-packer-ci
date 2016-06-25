02e39173aab6a19ecdbe15f45c571c98be24caa7
patrickcrocker <patrick.crocker@gmail.com>
2016-06-24 21:42:18 -0500

    fixed folder copy

--- tasks/prepare-release.sh
+++ tasks/prepare-release.sh
@@ -4,7 +4,7 @@
 set -e
 
 # copy the entire packer project to the task output
-cp -R concourse-packer/ prepare-release/
+cp -a concourse-packer/. prepare-release/
 
 # copy the concourse binary to the task output
 cp concourse-release/concourse_linux_amd64 prepare-release/cookbooks/concourse/files/.

d19c601125c7a18169fcda5aab45c82b7c046bef
patrickcrocker <patrick.crocker@gmail.com>
2016-06-24 22:52:19 -0500

    updated to use concourse version in ami_name

--- tasks/prepare-release.sh
+++ tasks/prepare-release.sh
@@ -8,3 +8,15 @@ cp -a concourse-packer/. prepare-release/
 
 # copy the concourse binary to the task output
 cp concourse-release/concourse_linux_amd64 prepare-release/cookbooks/concourse/files/.
+
+# get version from the source download (the only reason we downloaded it)
+VERSION=$(basename concourse-*.tar.gz .tar.gz | awk -F"-" '{ print $2 }')
+
+# append the version to the base config
+if [ ! -z "$CONFIG" ]; then
+  CONTENT="$CONFIG, \"concourse_version\": \"$VERSION\" "
+fi
+
+cat <<EOF > prepare-secrets/secrets.json
+{ $CONFIG }
+EOF
